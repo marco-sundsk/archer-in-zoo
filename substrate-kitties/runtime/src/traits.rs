05b7276b3f06b474b388b8c88e32c494fb961b49
Tang Bo Hao <tech@btang.cn>
2019-10-26 20:32:27 +0800

    - add is_item_owner to ItemTransfer trait - impl is_item_owner in kitties - use is_item_owner when add item

--- substrate-kitties/runtime/src/traits.rs
+++ substrate-kitties/runtime/src/traits.rs
@@ -2,6 +2,8 @@ use support::dispatch::Result;
 
 /// Means for interacting with transfering items between accounts
 pub trait ItemTransfer<AccountId, ItemId> {
+	/// Ensure item's owner
+	fn is_item_owner(who: &AccountId, item_id: ItemId) -> bool;
 	/// Transfer item from one to one
 	fn transfer_item(source: &AccountId, dest: &AccountId, item_id: ItemId) -> Result;
 }
